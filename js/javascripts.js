867a7098ce2b6b7814f611f7c69752a60ceb0937
EffortWay <effortway@gmail.com>
2022-01-25 12:18:42 +0600

    Finel

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,29 +1,9 @@
-$('.tab-menu ul li').click(function () {
+$('.tab-menu ul li').click(function(){
 
-	// $(this).closest('.tab-menu').siblings().find('.contain-items').show();
-	$(this).toggleClass('active')
-	$(this).siblings().removeClass('active')
-	$(this).closest('.tab-menu').siblings().find('.contain-items').removeClass('active fade').hide();
-
-	if( $(this).parent().children('li:nth-child(1)').hasClass('active') ){
-
-		$(this).closest('.tab-menu').siblings().find('.contain-items:nth-child(1)').addClass('active').show();
-
-	}
-
-	else if( $(this).parent().children('li:nth-child(2)').hasClass('active') ){
-
-		$(this).closest('.tab-menu').siblings().find('.contain-items:nth-child(2)').addClass('active').show();
-
-	}
-
-	else if( $(this).parent().children('li:nth-child(3)').hasClass('active') ) {
-
-		$(this).closest('.tab-menu').siblings().find('.contain-items:nth-child(3)').addClass('active').show();
-
-	}else {
-		$(this).closest('.tab-menu').siblings().children('.contain-items').hide();
-	}
-
+	var act = $(this).data('link');
+	$(this).addClass('active').siblings().removeClass('active')
+	$('.' + act).show().siblings().hide()
 })
+
+
 
